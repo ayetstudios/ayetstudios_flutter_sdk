7fb4104582da59bc66c4afc10d5e08af4b6ceeb7
Ivan Marinkovic <ivan.marinkovic@ayetstudios.com>
2023-09-08 14:17:42 +0200

    updated url_lancher logic

--- lib/ayetstudios_flutter_sdk.dart
+++ lib/ayetstudios_flutter_sdk.dart
@@ -1,23 +1,44 @@
 library ayetstudios_flutter_sdk;
 
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:io' show Platform;
+
+
 
 class AyetOfferwall extends StatelessWidget {
- Integer adslotId;
+ int adslotId;
  String externalIdentifier;
 
 AyetOfferwall({required this.adslotId,required this.externalIdentifier});
 
   @override
   Widget build(BuildContext context) {
-    String owUrl = 'https://www.ayetstudios.com/offers/web_offerwall/'+this.adslotId+'?external_identifier='+this.externalIdentifier;
+    String owUrl = 'https://www.ayetstudios.com/offers/web_offerwall/'+this.adslotId.toString()+'?external_identifier='+this.externalIdentifier+'&sdk=flutter&os_version='+Platform.operatingSystemVersion+'&os='+Platform.operatingSystem;
+
 
     return WebView(
           initialUrl: owUrl,
           allowsInlineMediaPlayback: true,
           onWebViewCreated: (controller) {},
           javascriptMode: JavascriptMode.unrestricted,
+          navigationDelegate: (NavigationRequest request) async {
+         final Uri url = Uri.parse(request.url);
+          if (request.url.contains('https://www.ayetstudios.com/offers/')) {
+            return NavigationDecision.navigate;
+          } else {
+            if (await canLaunchUrl(url)) {
+              await launchUrl(url, mode: LaunchMode.externalApplication);
+            } else {
+               print('ayeT-Studios Offerwall couldnt load outside link');
+             return NavigationDecision.prevent;
+            }
+            return NavigationDecision.prevent;
+          }
+        },
           onPageStarted: (String url) {
             print('ayeT-Studios Offerwall loading:$url');
           },
